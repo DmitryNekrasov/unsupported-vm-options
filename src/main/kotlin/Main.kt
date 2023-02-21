4e31862a3fe9840e3b8c3ec89510fe9cbae81d63
dnekrasov <dnekrasov@azul.com>
2023-02-21 18:30:41 +0400

    Add vm options generator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,29 @@
-import kotlin.io.path.Path
+import java.lang.RuntimeException
+import java.nio.file.Path
+
+class PrintFlagsFinalGenerator {
+    fun generate(path: String): Set<String> {
+        val java = Path.of(path, "bin", "java").toString()
+        val cmd = listOf(java, "-XX:+PrintFlagsFinal", "-version")
+        val process = ProcessBuilder().command(cmd).start()
+        val lines = process.inputStream.reader().use { it.readLines() }
+        val options = lines.slice(1..lines.lastIndex).map { it.split(' ').filter { s -> s.isNotEmpty() }[1] }.toSet()
+        val status = process.waitFor()
+        if (status != 0) {
+            throw RuntimeException("starting java failed")
+        }
+        return options
+    }
+}
 
 fun main(args: Array<String>) {
     if (args.size < 2) {
         throw IllegalArgumentException("Not enough arguments for the application to work. You need to pass 2 paths to the JDK.")
     }
-    val pathToJdkOld = Path(args[0])
-    val pathToJdkNew = Path(args[1])
-    println("Path to JDK old: $pathToJdkOld")
-    println("Path to JDK new: $pathToJdkNew")
+    val pathToJdkOld = args[0]
+    val pathToJdkNew = args[1]
+    val pffGenerator = PrintFlagsFinalGenerator()
+    val jdkOldOptions = pffGenerator.generate(pathToJdkOld)
+    val jdkNewOptions = pffGenerator.generate(pathToJdkNew)
+    println(jdkOldOptions.joinToString("\n"))
 }
