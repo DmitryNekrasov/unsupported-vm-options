495e371351d0fb111833c1534bf7dbbd053f8192
dnekrasov <dnekrasov@azul.com>
2023-02-21 18:47:15 +0400

    Refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,14 @@ import java.lang.RuntimeException
 import java.nio.file.Path
 
 fun generate(path: String): Set<String> {
-    val java = Path.of(path, "bin", "java").toString()
-    val cmd = listOf(java, "-XX:+PrintFlagsFinal", "-version")
+    val cmd = listOf(Path.of(path, "bin", "java").toString(), "-XX:+PrintFlagsFinal", "-version")
     val process = ProcessBuilder().command(cmd).start()
     val lines = process.inputStream.reader().use { it.readLines() }
-    val options = lines.slice(1..lines.lastIndex).map { it.split(' ').filter { s -> s.isNotEmpty() }[1] }.toSet()
     val status = process.waitFor()
     if (status != 0) {
         throw RuntimeException("starting java failed")
     }
-    return options
+    return lines.slice(1..lines.lastIndex).map { it.split(' ').filter { s -> s.isNotEmpty() }[1] }.toSet()
 }
 
 fun getNewOptions(old: Set<String>, new: Set<String>): List<String> {
