9a48091588e7864df5e47b1596792511436e36e0
dnekrasov <dnekrasov@azul.com>
2023-02-21 17:27:42 +0400

    Add args.size check

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import kotlin.io.path.Path
+
 fun main(args: Array<String>) {
-    args.forEach { println(it) }
+    if (args.size < 2) {
+        throw IllegalArgumentException("Not enough arguments for the application to work. You need to pass 2 paths to the JDK.")
+    }
+    val pathToJdkOld = Path(args[0])
+    val pathToJdkNew = Path(args[1])
+    println("Path to JDK old: $pathToJdkOld")
+    println("Path to JDK new: $pathToJdkNew")
 }
