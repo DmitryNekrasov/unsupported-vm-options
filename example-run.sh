e5c6ba0acc9ff8873155ca65dfb9aff5cb21bd10
dnekrasov <dnekrasov@azul.com>
2023-02-23 17:33:35 +0400

    Add jar and example bash

---
+++ example-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+java -jar unsupported-vm-options-1.0-jar-with-dependencies.jar \
+	/home/dnekrasov/dev/shipilev_builds/openjdk-17-fastdebug/ \
+	/home/dnekrasov/dev/shipilev_builds/openjdk-19-fastdebug/ \
+	/home/dnekrasov/dev/zing/openjdk/dev/hotspot/
+
